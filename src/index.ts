00ce8b9664661556837a9c11edeb884a99fbb399
Mariana Vieira <marianavcc@gmail.com>
2024-06-02 11:37:09 -0300

    Criado e configurado o arquivo de configuração typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+type heroi = {
+    name: string;
+    vulgo: String;
+};
+
+function printaObjeto(pessoa: heroi) {
+    console.log(pessoa);
+}
+
+printaObjeto({
+    name: "Super-Woman",
+    vulgo: "Super-Woman"
+});
+
